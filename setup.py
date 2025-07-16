dd388fd0e19e55d5e69c4e1c1e7488936fc050c7
Ubuntu <213869012+bgbyte@users.noreply.github.com>
2025-07-15 18:27:33 -0700

    ver #

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
    name='tsbuddy',
-   version='0.0.9',
+   version='0.0.10',
    packages=find_packages(),
    install_requires=[
        # Add dependencies here.
