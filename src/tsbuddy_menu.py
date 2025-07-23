99777f7992ffe210e27314c994af3dc0c7e77cb1
Ubuntu <213869012+bgbyte@users.noreply.github.com>
2025-07-22 18:34:23 -0700

    restructure menu, add art

--- src/tsbuddy_menu.py
+++ src/tsbuddy_menu.py
@@ -4,31 +4,64 @@ from src.tsbuddy import main as tsbuddy_main
 from src.extracttar.extracttar import main as extracttar_main
 from src.aosdl.aosdl import main as aosdl_main, lookup_ga_build, aosup
 
+ale_ascii = '''
+                  ...                   
+            .+@@@@@@@@@@@%=             
+         .#@@@@@@@@@@@@@@@@@@*.         
+       .%@@@@@@@@@@+. :%@@@@@@@#.       
+      *@@@@@@@@@@:  ++  @@@@@@@@@+      
+     #@@@@@@@@@=  =@@  -@@@@@@@@@@*     
+    %@@@@@@@@%. .%@@=  @@@@@@@@@@@@+    
+   =@@@@@@@@+  -@@@%. =@@@@%#%@@@@@@:   
+   #@@@@@@@.  -%  %#  #@@@@@@#@@@@@@*   
+   @@@@@@@.    =@@@  .@@@@@@@@+@@@@@#   
+   @@@@@%    -@@@@:  %@@@@@@@*#@@@@@#   
+   %@@@%.  .@@@@@@.  @@@@@@@@-@@@@@@*   
+   +@@@%- =@@@@@@*  +@@@@@@@@%@@@@@@=   
+   .@@@@@@@@@@@@@+  #@@@@@-.@@@@@@@@    
+    :@@@@@@@@@@@@+  #@@*: -@@@@@@@%.    
+     :@@@@@@@@@@@+      -@@@@@@@@%.     
+       +@@@@@@@@@@+..+%@@@@@@@@@=       
+        .*@@@@@@@@@@@@@@@@@@@@+         
+           .#@@@@@@@@@@@@@@*            
+               .-=++++=-.               
+'''
+
 def menu():
+    options = [
+        "Run tsbuddy",
+        "Run extracttar",
+        "Run aosdl",
+        "Run aosdl-ga (GA Build Lookup)",
+        "Run aosdl-up (AOS Upgrade)",
+        #"Exit"
+    ]
+    functions = [
+        tsbuddy_main,
+        extracttar_main,
+        aosdl_main,
+        lookup_ga_build,
+        aosup,
+        lambda: (print("Exiting."), sys.exit(0))
+    ]
     while True:
-        print("\n=== tsbuddy Menu ===")
-        print("1. Run tsbuddy")
-        print("2. Run extracttar")
-        print("3. Run aosdl")
-        print("4. Run aosdl-ga (GA Build Lookup)")
-        print("5. Run aosdl-up (AOS Upgrade)")
-        print("6. Exit")
+        #print("\n       (•‿•)  Hey there, buddy!")
+        print(ale_ascii)
+        print("\n   ( ^_^)ノ  Hey there, tsbuddy is at your service!")
+        print("\n=== 🛎️  ===")
+        for idx, option in enumerate(options, 1):
+            print(f"{idx}. {option}")
+        print("\n0. Exit  (つ﹏<) \n")
         choice = input("Select an option: ").strip()
-        if choice == '1':
-            tsbuddy_main()
-        elif choice == '2':
-            extracttar_main()
-        elif choice == '3':
-            aosdl_main()
-        elif choice == '4':
-            lookup_ga_build()
-        elif choice == '5':
-            aosup()
-        elif choice == '6':
-            print("Exiting.")
+        if choice.isdigit() and 1 <= int(choice) <= len(functions):
+            #print(f"\n   ( ^_^)ノ⌒☆   \n")
+            print(f"\n   ( ^_^)ノ🛎️   \n")
+            functions[int(choice) - 1]()
+        elif choice == '0':
+            print("Exiting...\n\n  (×_×) \n")
             sys.exit(0)
         else:
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
-    menu() 
+    menu()
