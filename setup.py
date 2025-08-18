a12eac169ea9d21f7c305c8b3f9eb954d5d5f6ba
Ubuntu <213869012+bgbyte@users.noreply.github.com>
2025-08-18 13:29:05 -0700

    exclude .env

--- setup.py
+++ setup.py
@@ -33,4 +33,5 @@ setup(
    long_description_content_type='text/markdown',
    description = "Tech Support Buddy is a versatile Python module built to empower developers and IT professionals in resolving technical issues. It provides a suite of Python functions designed to efficiently diagnose and resolve technical issues by parsing raw text into structured data, enabling automation and data-driven decision-making.",
    include_package_data=True,
+   exclude_package_data={"": [".env"]},
 )
