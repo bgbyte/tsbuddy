c7a2740dc18cc0be8207f023a038643b6a988ee1
bgbyte <213869012+bgbyte@users.noreply.github.com>
2025-09-22 16:22:15 -0700

    fixed bugs

--- __main__.py
+++
@@ -1,4 +0,0 @@
-# from src.tsbuddy.tsbuddy_menu import menu
-
-# if __name__ == "__main__":
-#     menu()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
    name='tsbuddy',
-   version='0.0.30',
+   version='0.0.31',
    packages=find_packages(where='src'),
    package_dir={'': 'src'},
    install_requires=[
