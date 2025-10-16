5570830e597e1f999bdd3433ccf9dd065cd17228
bgbyte <213869012+bgbyte@users.noreply.github.com>
2025-10-15 19:23:27 -0700

    fixed smemcap bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
    name='tsbuddy',
-   version='0.0.37',
+   version='0.0.38',
    packages=find_packages(where='src'),
    package_dir={'': 'src'},
    install_requires=[
